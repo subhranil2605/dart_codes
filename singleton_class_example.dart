9461ebd617eba84a9780d08e4eb1b0f3c164a423
Subhranil Sarkar <57196419+subhranil2605@users.noreply.github.com>
2022-06-17 00:37:52 +0530

    Null safety added
    Singleton class unless we meet again!!!

--- singleton_class_example.dart
+++ singleton_class_example.dart
@@ -1,17 +1,15 @@
 // Example of a singleton class
 class DatabaseHelper {
-  
   // Singleton databasehelper
-  static DatabaseHelper _databaseHelper;
-  
+  static DatabaseHelper? _databaseHelper;
+
   // Named constructor to create an instance
   DatabaseHelper._createInstance();
-  
+
+  // returns new instance if and only there are not any existing instances!!!
   factory DatabaseHelper() {
-    if (_databaseHelper == null) {
-      _databaseHelper = DatabaseHelper._createInstance();
-    }
-    return _databaseHelper;
+    _databaseHelper ??= DatabaseHelper._createInstance();
+    return _databaseHelper!;
   }
 }
 
@@ -19,23 +17,23 @@ class DatabaseHelper {
 class Note {
   final int id;
   final String title;
-  
+
   Note(this.id, this.title);
 }
-
-
 
 void main() {
   DatabaseHelper databaseHelper1 = DatabaseHelper();
   DatabaseHelper databaseHelper2 = DatabaseHelper();
-  
+  DatabaseHelper databaseHelper3 = DatabaseHelper();
+
   print(databaseHelper1.hashCode);
   print(databaseHelper2.hashCode);
-  
+  print(databaseHelper3.hashCode);
+
   Note note1 = Note(1, "First Note");
   Note note2 = Note(2, "Second Note");
-  
+
+  // as they are not maintaining singleton pattern therefore their hashCode will be different
   print(note1.hashCode);
   print(note2.hashCode);
-  
 }
